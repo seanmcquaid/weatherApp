26a05368b21d44eeb2cb5d09458415c9600b9922
seanmcquaid <sean.mcquaid2@verizon.net>
2018-12-20 14:29:42 -0500

    used canvas to create an arc based on temp

--- scripts.js
+++ scripts.js
@@ -1,39 +1,58 @@
-$("#weather-form").submit((event)=>{
-    // prevents default behavior
-    // prevents from reloading the page
-    event.preventDefault();
-    const zip = $(".zip-code").val();
-    
-    const weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?zip=${zip},us&units=imperial&appid=${apiKey}`;
+$('#weather-form').submit((e)=>{
+    e.preventDefault();
+    // console.log("User submitted!")
+    const zip = $('.zip-code').val();
+    // console.log(zip);
+    const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=imperial`;
     // we got the zip code
-    // we built the url
-    // go get the JSON
-    // $.getJSON is short for $.ajax
+    // we built the URL.
+    // GO GET THE JSON
+    // $.getJSON, is shorthand for $.ajax
     // it expects 2 args:
-    // 1) URL to fetch the json form
-    // 2) the callback to run when the ajax comes back
-
-$.getJSON(weatherUrl, (weatherData) =>{
-    const locationInfo = {
-        city: weatherData.city.name,
-        weatherIcon: weatherData.icon,
-        temp: weatherData.temp,
-        fromDate: "",
-        toDate:"",
-    };
-    let locationMiddleHTML = `
-        <div class="city-name">Here is the forecast for ${locationInfo.city}!</div>
-    `
-    $(".middle").html(locationMiddleHTML);
-
-    let locationBottomHTML = `
-        <div class="${locationInfo.city} col-4"
-            <div class="weather-icon"><img class="symbol" src="${locationInfo.weatherIcon}.png"></div>
-            <div class="temp">${locationInfo.temp}</div>
-        </div>
-    `
-    $(".bottom").html(locationBottomHTML);
-
+    // 1. URL to fetch the JSON from
+    // 2. the callback to run when the AJAX is back
+    $.getJSON(weatherUrl,(weatherData)=>{
+        console.log(weatherData);
+        const currTemp = weatherData.main.temp;
+        const temps = {
+            curr: weatherData.main.temp,
+            max: weatherData.main.temp_max,
+            min: weatherData.main.temp_min
+        }
+        const newHTML = `<img src="https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png" />
+        <div>The temp in ${weatherData.name} is currently ${temps.curr} &deg;
+        `
+        $('.weather-data').html(newHTML);
+        animateTemp(0,currTemp);
     });
 });
 
+const canvas = document.getElementById("canvas");
+let context = canvas.getContext("2d");
+let currentPercent = 0;
+
+function animateTemp(currentArc, currentTemp){
+    context.lineWidth = 5;
+    context.strokeColor = "#ffff00";
+    context.beginPath();
+    // full inner circle
+    context.fillStyle = "#ccc";
+    context.arc(155, 75, 70, 0, Math.PI * 2);
+    context.closePath();
+    context.fill();
+
+    // outer line
+    context.beginPath();
+    context.arc(155, 75, 75, Math.PI * 1.5, Math.PI * 2 * currentArc + Math.PI * 1.5);
+    context.stroke();
+
+    // update the arc percentage until we hit currtemp
+    currentPercent++;
+    if(currentPercent < currentTemp){
+        // we need to keep drawing
+        // requestAnimationFrame is like a while loop for canvas
+        requestAnimationFrame(()=>{
+            animateTemp(currentPercent/100, currentTemp);
+        })
+    }
+};
